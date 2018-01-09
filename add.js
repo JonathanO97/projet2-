7997ff8dc88d851a4be2a46ef0bced8ce7210e94
Jonathan <jonathanoneill97@hotmail.com>
2018-01-09 10:30:49 -0500

    JON: Modified 'add' function to return the sum of two arguments

--- add.js
+++ add.js
@@ -1,8 +1,12 @@
-let add = function(numberString) {
-  if(numberString.length === 0) 
+let add = function(numberStringOne, numberStringTwo) { 
+  
+  if(typeof numberStringOne !== "undefined" && typeof numberStringTwo !== "undefined")
+		return numberStringOne + numberStringTwo;
+	
+  if(numberStringOne.length === 0) 
 	  return 0; 
-  else 
-	  return numberString; 
+  else
+	  return numberStringOne; 
 }
 
 module.exports = add;
